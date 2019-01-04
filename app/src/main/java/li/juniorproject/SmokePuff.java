3d19bd32c54e8837e82a50b0fb4a372b19da510f
valkyriean <aegis_fate@outlook.com>
2019-01-05 01:20:05 +0800

    ignore

---
+++ app/src/main/java/li/juniorproject/SmokePuff.java
@@ -0,0 +1,33 @@
+package li.juniorproject;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by AlexanderLI on 5/21/16.
+ */
+public class SmokePuff extends GameObject {
+    public int r;
+
+    public SmokePuff(int x, int y)
+    {
+        r = 5;
+        setX(x);
+        setY(y);
+    }
+
+    public void update(){
+        x -= 10;
+    }
+
+    public void draw(Canvas canvas){
+        Paint paint = new Paint();
+        paint.setColor(Color.GRAY);
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawCircle(x-r, y-r,r,paint);
+        canvas.drawCircle(x-r+2, y-r-2, r, paint);
+        canvas.drawCircle(x-r+4, y-r+1, r, paint);
+    }
+}
